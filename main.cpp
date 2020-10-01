fe650d714cb1989c13dd585758be4ead2c0255aa
Daniel Paskalev <dpaskal1@binghamton.edu>
2020-09-30 20:40:52 -0400

    size

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    cs540::Map<std::string, int> map;
+    cs540::Map<int, int> map;
+    cs540::Map<char, int> cmap = {{'a', 1}, {'b',2}};
     std::cout << map.i << std::endl;
-    std::cout << map.size() << std::endl;
-
+    std::cout << cmap.size() << std::endl;
     return 0;
 }
--- map.hpp
+++ map.hpp
@@ -8,13 +8,23 @@ namespace cs540 {
 	struct Map {
 		// public by default
 		typedef std::pair<key_t, mapped_t> val_t;
-		size_t _size;
-		int i;	
+		size_t _size = 0;
+		int i;
 		Map() { // constructor
 			i = 987654321;
-			_size = 0;
+			// _size = 0;
 		}
-
+		Map(const Map& copyMap) {
+			_size = copyMap._size;
+		}
+		Map(std::initializer_list<val_t> elems) {
+			// _size = 0;
+			std::cout << "initializer list constructor: " << std::endl;
+			for(auto e: elems) {
+				_size++;
+				std::cout << e.first << " " << e.second << std::endl;
+			}
+		}
 		~Map() {	// destructor
 			;
 		}
