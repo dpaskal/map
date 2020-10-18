b4e80e548b65765c54e087d7c71b50f9cc19b6fd
Daniel Paskalev <dpaskal1@binghamton.edu>
2020-10-18 02:14:49 -0400

    internal table

--- map.hpp
+++ map.hpp
@@ -23,32 +23,34 @@ namespace cs540 {
 
 	template <typename key_t, typename mapped_t>
 	struct Map {
-		// public by default
-		typedef std::pair<key_t, mapped_t> val_t;
-		size_t _size = 0;
-		Map() {		// constructor
-			;
-		}
-		Map(const Map& copyMap) {
-			_size = copyMap._size;
-		}
-		Map(std::initializer_list<val_t> elems) {
-			std::cout << "initializer list constructor: " << std::endl;
-			for(val_t e: elems) {
-				++_size;
-				std::cout << e.first << " " << e.second << std::endl;
+		private:
+			node<key_t, mapped_t> *table;
+		public:
+			typedef std::pair<key_t, mapped_t> val_t;
+			size_t _size = 0;
+			Map() {		// constructor
+				;
 			}
-		}
-		~Map() {	// destructor
-			;
-		}
-			
-		size_t size() {
-			return _size;
-		}
-		bool empty() {
-			return _size == 0;
-		}
+			Map(const Map& copyMap) {
+				_size = copyMap._size;
+			}
+			Map(std::initializer_list<val_t> elems) {
+				std::cout << "initializer list constructor: " << std::endl;
+				for(val_t e: elems) {
+					++_size;
+					std::cout << e.first << " " << e.second << std::endl;
+				}
+			}
+			~Map() {	// destructor
+				;
+			}
+				
+			size_t size() {
+				return _size;
+			}
+			bool empty() {
+				return _size == 0;
+			}
 
 	}; // struct Map
 }
